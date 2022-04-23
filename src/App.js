e27c10955bde17cb9b89113844479ceb0c5c5112
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-04-23 12:05:29 +0100

    Setting up Homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Homepage from './components/Homepage/Homepage';
 
 function App() {
   return (
     <div className="App">
       <h1>Restaurant Website by Group 38</h1>
+      <Homepage />
     </div>
   );
 }
---
+++ src/components/Homepage/Homepage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Homepage() {
+  return (
+    <div>
+        <h2>This is the homepage</h2>
+    </div>
+  )
+}
+
+export default Homepage
