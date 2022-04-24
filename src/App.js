dc20e3cb1e68a5501d9d95b6a9d7dea5520fb79e
Ajaye Favour <tehilafavourite@gmail.com>
2022-04-24 12:31:04 -0900

    Menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
 import Menu from './components/menu/Menu';
 
 
 function App() {
   return (
-    <div className="App">
-      <Menu />
-    </div>
+    <Routes className="App">
+      <Route path='Menu' element={<Menu />}></Route>
+      
+    </Routes>
   );
 }
 
