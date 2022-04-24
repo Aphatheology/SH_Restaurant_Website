9c22346fb9c20ec6adbcd106230f873c804b0a35
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-04-24 17:15:01 +0100

    Fix Routing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import './App.css';
-import { Routes, Route } from 'react-router-dom';
+import Footer from './components/Footer/Footer';
+import Homepage from './components/Homepage/Homepage';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import About from './components/About';
+import Contact from './components/Contact';
 import Menu from './components/menu/Menu';
-
+import Navbar from './components/Homepage/Navbar';
 
 function App() {
   return (
-    <Routes className="App">
-      <Route path='Menu' element={<Menu />}></Route>
-      
-    </Routes>
+    <div className="App">
+
+
+      {/* <h1>Restaurant Website by Group 38</h1>
+      <Homepage /> */}
+
+      <Router>
+            <Navbar />
+            <Routes>
+                <Route exact path="/" element={<Homepage />}></Route>
+                <Route exact path="/about" element={<About />}></Route>
+                <Route exact path="/contact" element={<Contact />}></Route>
+                <Route exact path="/menu" element={<Menu />}></Route>
+            </Routes>
+
+        </Router>
+<Footer />
+    </div>
   );
 }
 
