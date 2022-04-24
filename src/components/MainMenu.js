d143ceafafd56097f589ed99ca9acbf9ae2882cf
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-04-24 21:38:32 +0100

    Revert Menu

--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Menu from './menu/Menu'
 
 function MainMenu() {
   return (
     <div>
-        <Menu />
+        <h2>Menu</h2>
     </div>
   )
 }
--- src/constants/images.js
+++
@@ -1,4 +0,0 @@
-import menu from '../assets/menu.png';
-import spoon from '../assets/spoon.png';
-
-export default {menu, spoon}
