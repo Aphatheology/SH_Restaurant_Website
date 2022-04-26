ec6477240e2f1f402954c844daa6138baf6fc8cf
Abdulkareem Mustapha <aphatheology@gmail.com>
2022-04-26 10:41:03 +0100

    Add AboutUs Page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import AboutUs from './About/AboutUs'
 
 function About() {
   return (
     <div>
-        <h2>About page</h2>
+        {/* <h2>About page</h2> */}
+        <AboutUs />
     </div>
   )
 }
---
+++ src/components/About/AboutUs.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import './AboutUs.css'
+
+function AboutUs() {
+  return (
+    <div className='aboutus'>
+        <h2>SH Restaurant - Group 38</h2>
+            <p>
+                We are a group of 8 members with active 5 Members. <br></br>
+                Below are the details of Members involved in this project
+            </p>
+
+            <div className='container'>
+                <div>
+                    <h3>Abdulkareem Mustopha - Team Lead</h3>
+                    <p>
+                        <ul>
+                            <li>Design the Homepage, Navbar and About Us Page</li>
+                        </ul>
+                    </p>
+                </div>
+
+                <div>
+                    <h3>Favour - Team Member</h3>
+                    <p>
+                        <ul>
+                            <li>Design the Menu Page</li>
+                        </ul>
+                    </p>
+                </div>
+
+                <div>
+                    <h3>Emmanuel - Team Member</h3>
+                    <p>
+                        <ul>
+                            <li>Design the Footer</li>
+                        </ul>
+                    </p>
+                </div>
+
+                <div>
+                    <h3>Chimdi - Team Member</h3>
+                    <p>
+                        <ul>
+                            <li>Design the Contact Us Page</li>
+                        </ul>
+                    </p>
+                </div>
+
+                <div>
+                    <h3>Moses - Team Member</h3>
+                    <p>
+                        <ul>
+                            <li>Design the About Us Page</li>
+                        </ul>
+                    </p>
+                </div>
+
+            </div>
+
+            
+
+            
+
+            
+
+            
+
+            
+    </div>
+  )
+}
+
+export default AboutUs
